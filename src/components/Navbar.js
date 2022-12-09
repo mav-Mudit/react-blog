58d38741f0802484b841ed70f0113a7712764da1
Mudit <mudittandon34@gmail.com>
2022-12-09 18:40:24 +0530

    Implemented Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,17 @@
-// import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
-
-
- function Navbar() {
+function Navbar() {
   return (
     <div>
-   Navbar
+      <ul id="nav">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/create-post">Create Post</Link>
+        </li>
+      </ul>
     </div>
-  )
+  );
 }
-export default Navbar
+export default Navbar;
