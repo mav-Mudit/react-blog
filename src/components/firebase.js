33c307db5b0ada037f492338aa0c9a7e6161a135
Mudit <mudittandon34@gmail.com>
2022-12-09 16:00:53 +0530

    Initial Commit:- Setting Up the Project

---
+++ src/components/firebase.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase/compat/app'
+import 'firebase/compat/firestore'
+
+import { initializeApp } from "firebase/compat/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBbpLaMTAatTW9MM1GCOoyDBDDAWb19inU",
+  authDomain: "react-hooks-blog-ebcbc.firebaseapp.com",
+  projectId: "react-hooks-blog-ebcbc",
+  storageBucket: "react-hooks-blog-ebcbc.appspot.com",
+  messagingSenderId: "788600818354",
+  appId: "1:788600818354:web:177ba056fd95bb0a6b17ab"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const firestore=firebase.firestore()
